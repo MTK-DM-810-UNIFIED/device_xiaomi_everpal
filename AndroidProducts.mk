e9c350598aa463d3229eeb0761acc29b5e5d0262
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2024-02-14 15:47:59 +0800

    everpal: adapt for RisingOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_everpal.mk
+    $(LOCAL_DIR)/rising_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
+    rising_everpal-user \
+    rising_everpal-userdebug \
+    rising_everpal-eng
--- rising_everpal.mk
+++ rising_everpal.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
@@ -20,6 +20,16 @@ PRODUCT_DEVICE := everpal
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := everpal
+
+# RisingOS flags
+RISING_CHIPSET := "MT6833P"
+RISING_MAINTAINER := "Weaponmasterjax and Jignesh2023"
+RISING_BATTERY := 5000mah
+TARGET_CORE_GMS := true
+TARGET_ENABLE_BLUR := true
+
+# GMS build flags
+WITH_GMS := true
 
 # Build info
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
